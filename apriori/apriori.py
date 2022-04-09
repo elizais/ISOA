0044105bfbd503dd2048b0927cb609ba90ec6aaa
Elizaveta <elizaveta1902@gmail.com>
2022-04-09 17:07:43 +0500

    add colculation second dataset

--- apriori/apriori.py
+++ apriori/apriori.py
@@ -16,48 +16,58 @@ from IPython.display import display, HTML
 import apyori
 
 
-def apriori_dataset_market_basket_optimisation() -> pd.DataFrame:
-    dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
-    print(dataset)
-
-    dataset.fillna(method='ffill', axis=1, inplace=True)
+def apriori_read_dataset(file_name: str, header='infer') -> pd.DataFrame:
+    dataset = pd.read_csv(file_name, header=header)
     print(dataset)
     return dataset
 
 
-def apriori_dataset_formation(dataset: pd.DataFrame) -> list:
+def apriori_fill_dataset(dataset: pd.DataFrame, axis) -> pd.DataFrame:
+    dataset.fillna(method='ffill', axis=axis, inplace=True)
+    print(dataset)
+    return dataset
+
+
+def apriori_dataset_formation(dataset: pd.DataFrame, transactions_length: int) -> list[list[str]]:
     transactions = []
-    for i in range(dataset.shape[2]):
-        transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+    for i in range(dataset.shape[0]):
+        transactions.append([str(dataset.values[i, j]) for j in range(transactions_length)])
     return transactions
 
 
-def apriori_result(transactions: list, min_support: float, min_length: int, min_confidence: float) -> list[tuple]:
+def apriori_result(
+        transactions: list[list[str]],
+        min_support: float, min_length:
+        int, min_confidence: float,
+        min_lift: int) -> list[tuple]:
     # min_support -- минимальный support для правил (dtype = float).
     # min_confidence -- минимальное значение confidence для правил (dtype = float)
     # min_lift -- минимальный lift (dtype = float)
     # max_length -- максимальная длина itemset (вспоминаем про k-itemset)  (dtype = integer)
-    return list(
-        apyori.apriori(transactions, min_support=min_support, min_length=min_length, min_confidence=min_confidence))
+    return list(apyori.apriori(
+        transactions,
+        min_support=min_support,
+        min_length=min_length,
+        min_confidence=min_confidence,
+        min_lift=min_lift))
 
 
 def apriori_experiment(
         dataset: pd.DataFrame,
         min_supports: list[float],
         min_length: int,
-        min_confidence: float
+        min_confidence: float,
+        min_lift: int,
+        transactions_length: int
 ) -> tuple[list[float], list[int]]:
-    transactions = apriori_dataset_formation(dataset)
+    transactions = apriori_dataset_formation(dataset, transactions_length)
 
-    # min_supports = [0.01, 0.03, 0.05, 0.1, 0.15]
-    # min_length = 2
-    # min_confidence = 0.1
     elapsed_time = []
     length_result = []
 
     for min_support in min_supports:
         t0 = time.time()
-        result = apriori_result(transactions, min_support, min_length, min_confidence)
+        result = apriori_result(transactions, min_support, min_length, min_confidence, min_lift)
         print(len(result))
         t1 = time.time()
         print("Time elapsed: ", t1 - t0)
@@ -70,30 +80,13 @@ def apriori_experiment(
     return elapsed_time, length_result
 
 
-def apriori_output_json(result: list[tuple]) -> pd.DataFrame:
-    output = []
-
-    for RelationRecord in result:
-        o = StringIO()
-        apyori.dump_as_json(RelationRecord, o)
-        output.append(json.loads(o.getvalue()))
-
-    data_df = pd.DataFrame(output)
-    # и взгялнем на итоги
-    pd.set_option('display.max_colwidth', None)
-
-    display(HTML(data_df.to_html()))
-
-    return data_df
-
-
 def apriori_normal_output(result: list[tuple]) -> list[list]:
     apriori_output = []
 
     for elem in result:
         output = [round(elem.support, 4)]
 
-        for item in output.items:
+        for item in elem.items:
             output.append(item)
         apriori_output.append(output)
 
--- apriori/main.py
+++ apriori/main.py
@@ -1,7 +1,43 @@
-import pandas as pd
+from apriori import *
 
 if __name__ == '__main__':
-    dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
-    dataset.fillna(method='ffill', axis=1, inplace=True)
-    print(dataset)
+    # 1
+    dataset = apriori_read_dataset('Market_Basket_Optimisation.csv', header=None)
+    dataset = apriori_fill_dataset(dataset, axis=1)
+    print(dataset.shape)
 
+    min_supports = [0.01, 0.03, 0.05, 0.1, 0.15]
+    min_length = 2
+    min_confidence = 0.1
+    min_lift = 2
+    elapsed_time, length_result = apriori_experiment(dataset, min_supports, min_length, min_confidence, min_lift, 20)
+
+    transactions = apriori_dataset_formation(dataset, 20)
+    result = apriori_result(
+        transactions,
+        min_support=0.01,
+        min_length=min_length,
+        min_confidence=min_confidence,
+        min_lift=min_lift)
+    # print(apriori_normal_output(result))
+
+    apriori_plots_result(min_supports, elapsed_time, length_result)
+
+    # 2
+    dataset = apriori_read_dataset('Starbucks satisfactory survey.csv', header=None)
+    dataset = dataset[1:]
+    dataset = apriori_fill_dataset(dataset,  1)
+
+    elapsed_time, length_result = apriori_experiment(dataset, min_supports, min_length, min_confidence, min_lift, 10)
+
+    transactions = apriori_dataset_formation(dataset, 10)
+    result = apriori_result(
+        transactions,
+        min_support=0.01,
+        min_length=min_length,
+        min_confidence=min_confidence,
+        min_lift=min_lift)
+
+    # print(apriori_normal_output(result))
+
+    apriori_plots_result(min_supports, elapsed_time, length_result)
